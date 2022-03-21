ee83afcae5dd4c0b917f4caa509f1fe431970d11
lolekgk <karol.gajda97@gmail.com>
2022-03-21 20:56:59 +0100

    Add abc_mirror() function

--- keymaker.py
+++ keymaker.py
@@ -11,30 +11,23 @@ def pad_up_to(word, shift, n):
     """
     >>> pad_up_to('abb', 5, 11)
     'abbfggkllpq'
-    * zwrocic slowo o dlugosci n
-    * n moze byc mniejsze od dlugosci slowa
-    * dla n mniejszego od word zwracamy czesc worda
-    * dla n rownego word zwracamy word
-    * dla n wiekszego od word zwracamy word+padup'y
     """
     padded_up = ''
     while len(padded_up) <= n:
         padded_up += word
         word = shift_characters(word, shift)
-    print("aaaccceeegggiiikkkmmmoooqqqsssuuuwwwyyyaaaccceeegggiiikkkmmmoooqqqsssuuuwwwyyyaaaccceeegggiiikkkmmmo" == padded_up[:n])
     return padded_up[:n]
 
-    
-        
-      
-print(pad_up_to('aaa', 2, 100))
 
 def abc_mirror(word):
     """
     >>> abc_mirror('abcd')
     'zyxw'
     """
-    pass
+    mirrored_word = ''
+    for i in range(len(word)):
+        mirrored_word += chr(ord('z') - (ord(word[i]) - ord('a')))
+    return mirrored_word
 
 
 def create_matrix(word1, word2):
